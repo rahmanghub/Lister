162fb86ced712d1d843ae59e7c326db3e70fb40e
rahmanghub <indmabdulrahman@gmail.com>
2024-07-17 18:59:25 +0530

    DB Chsnge

--- learn/settings.py
+++ learn/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'learn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'koyebdb',
+        'USER': 'koyeb-adm',
+        'PASSWORD': 'UBDSjur9pam6',
+        'HOST': 'ep-crimson-cherry-a297a967.eu-central-1.pg.koyeb.app',
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
