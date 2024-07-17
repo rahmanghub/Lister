6bb3a64b7e6ff013871103b1d24b63dccc4353d9
rahmanghub <indmabdulrahman@gmail.com>
2024-07-17 20:30:28 +0530

    Final Comit

--- learn/settings.py
+++ learn/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rz3()h$6-&7-2^1x1ac!a7zk(6rc8#hs=u752jr)*!xw**dp%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','lister-6osa.onrender.com']
 
